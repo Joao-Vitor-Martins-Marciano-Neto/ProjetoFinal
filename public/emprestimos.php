3f48dcb8f81d3b69110d7f46350d51b8e8e0fd28
erickgomes-maker <erick.gomes@estudande.ifgoiano.edu.br>
2025-11-03 11:58:17 -0300

    Arquivos da pasta flow foram criados

--- public/emprestimos.php
+++ public/emprestimos.php
@@ -1,3 +1,33 @@
 <?php
-    
+require_once __DIR__ . '../../config/db.php';
+
+//Login...verificar
+//Pegar ISBN
+
+session_start();
+
+if(isset($_SESSION['logado'])) 
+{
+  $isbn = $_GET['ISBN'];
+
+  // Verificar disponibilidade
+  $resultado=pg_query_params(
+        $dbconn,
+        "SELECT disponibilidade FROM livros as l WHERE l.isbn = $1",
+        [$isbn]
+  );
+
+  if($resultado[0]['disponibilidade'] == TRUE) 
+  {
+    // Alterar disponibilidade
+    pg_exec($dbconn,"ALTER TABLE livros as l SET disponibilidade=FALSE WHERE l.isbn = $1",
+    [$isbn]);
+ 
+    //Código abaixo deve ser corrigido
+    pg_exec($dbconn,"ALTER TABLE emprestimo as e SET id_usuario=$1 WHERE e.isbn=$2", []);
+  } else {
+     echo "Livro indisponível";
+  }
+
+} 
 ?>
---
+++ public/flow/authcadastro.php
@@ -0,0 +1,53 @@
+<?php
+require_once __DIR__ . '../../config/db.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+  {
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $senha = $_POST['senha'];
+  $confirmar_senha = $_POST['confirmar_senha'];
+}
+
+//Verificação se existe email no BD
+$result = pg_query_params(
+  $dbconn, 
+  "SELECT nome FROM usuarios WHERE email = $1", 
+  [$_POST['email']]
+);
+$dados = pg_fetch_all($result);
+
+if(!empty($dados))
+{
+   $msg = "Esse email já está cadastrado! <br>";
+}
+//Exigência para no mínimo 3 caracteres no nome
+if (strlen(($nome)) < 3) {
+  $msg = $msg . "O nome deve conter no mínimo 3 caracteres. <br>";
+}
+//Verificação dos campos de senha
+if ($senha !== $confirmar_senha) {
+  $msg = $msg . "As senhas não coincidem! <br>";
+} else {
+  $senha = password_hash($senha, PASSWORD_DEFAULT);
+}
+//Exigência para no mínimo 6 caracteres na senha e um número (segurança)
+if (strlen($senha) < 6) {
+  $msg=$msg . "A senha deve conter mínimo 6 caracteres!<br>";
+}
+if (!preg_match('/[0-9]/', $senha)) {
+  $msg = $msg . "A senha deve conter mínimo um número";
+}
+//Emitir mensagens de erro
+if(isset($msg))
+{
+  header("Location: ../cadastro.php",true,302);
+} else
+//Inserir dados no BD
+ {
+  $result = pg_query_params($dbconn,"INSERT INTO usuarios (nome, email , senha_hash ) VALUES ($1, $2, $3)",[ $nome , $email ,$senha]);
+}
+//Redirecionamento para outra página 
+header("Location: ../index.php");
+
+?>
